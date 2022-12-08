ace024bb465adf933b5a956a8377016f1f037593
duped0 <107189679+duped0@users.noreply.github.com>
2022-12-08 17:28:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+  canvas = createCanvas(550, 550);
+  canvas.position(900,100);
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
+
+function draw() {
+background('#969A97');
+  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+  textSize(10);
+  fill('grey');
+  text('archit patel',10,10);
+
+}
